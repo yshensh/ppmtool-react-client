23cbebe80b96700d84210e37bcc6268d95bb719e
Yi Shen <yshensh@gmail.com>
2020-04-23 23:01:01 -0400

    delete an existing project

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT} from "./types";
 
 
 export const createProject = (project, history) => async dispatch => {
     try {
-        const res = await axios.post
-        ("http://localhost:8080/api/project", project)
+        const res = await axios.post("/api/project", project)
         history.push("/dashboard");
 
         // everytime we create a valid new project, do cleanup on errors
@@ -22,7 +21,7 @@ export const createProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-    const res = await axios.get("http://localhost:8080/api/project/all")
+    const res = await axios.get("/api/project/all")
     dispatch ({
         type: GET_PROJECTS,
         payload: res.data
@@ -33,7 +32,7 @@ export const getProjects = () => async dispatch => {
 export const getProject = (id, history) => async dispatch => {
 
     try {
-        const res = await axios.get(`http://localhost:8080/api/project/${id}`)
+        const res = await axios.get(`/api/project/${id}`)
         dispatch ({
             type: GET_PROJECT,
             payload: res.data
@@ -41,5 +40,20 @@ export const getProject = (id, history) => async dispatch => {
     } catch (error) {
         history.push("/dashboard");
     }
-    
+
+};
+
+
+export const deleteProject = id => async dispatch => {
+    if(
+        window.confirm(
+            "Are you sure that you want to delete the project? "
+        )
+    ) {
+        await axios.delete(`/api/project/${id}`)
+        dispatch ({
+            type: DELETE_PROJECT,
+            payload: id
+        });
+    }
 };
