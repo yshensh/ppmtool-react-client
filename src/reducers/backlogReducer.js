0fe85283ce8bc8517ba35cd5ba7ab0f88a21c60b
Yi Shen <yshensh@gmail.com>
2020-07-10 18:03:43 -0400

    types and reducers for project tasks

---
+++ src/reducers/backlogReducer.js
@@ -0,0 +1,32 @@
+import {GET_BACKLOG, GET_PORJECT_TASK, DELETE_PROEJCT_TASK} from "../actions/types";
+
+const initialState = {
+        proejct_tasks: [],
+        project_task: {}
+}
+
+export default function(state=initialState, action) {
+    switch(action.type) {
+
+        case GET_BACKLOG:
+            return {
+                ...state,
+                proejct_tasks: action.payload
+            }
+
+        case GET_PORJECT_TASK:
+            return {
+                ...state,
+                project_task: action.payload
+            }
+
+        case DELETE_PROEJCT_TASK:
+            return {
+                ...state
+            }
+
+        default:
+            return state;
+    }
+
+}
